8c7498f594ce063ba05cf6e0416e5ae9fa185eda
Jiri Slaby <jirislaby@gmail.com>
2021-10-18 08:46:29 +0200

    ui/mainwindow: hide tree header

--- trans/stansec_cs.ts
+++ trans/stansec_cs.ts
@@ -9,118 +9,118 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/ui/mainwindow.ui" line="50"/>
+        <location filename="../src/ui/mainwindow.ui" line="54"/>
         <source>Report Navigation &amp;&amp; Controls</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/ui/mainwindow.ui" line="56"/>
+        <location filename="../src/ui/mainwindow.ui" line="60"/>
         <source>BUG</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/ui/mainwindow.ui" line="63"/>
+        <location filename="../src/ui/mainwindow.ui" line="67"/>
         <source>«</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/ui/mainwindow.ui" line="76"/>
+        <location filename="../src/ui/mainwindow.ui" line="80"/>
         <source>First</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/ui/mainwindow.ui" line="83"/>
+        <location filename="../src/ui/mainwindow.ui" line="87"/>
         <source>»</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/ui/mainwindow.ui" line="90"/>
+        <location filename="../src/ui/mainwindow.ui" line="94"/>
         <source>Last</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/ui/mainwindow.ui" line="97"/>
+        <location filename="../src/ui/mainwindow.ui" line="101"/>
         <source>Not Bug</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/ui/mainwindow.ui" line="104"/>
+        <location filename="../src/ui/mainwindow.ui" line="108"/>
         <source>Clear</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/ui/mainwindow.ui" line="111"/>
+        <location filename="../src/ui/mainwindow.ui" line="115"/>
         <source>&amp;Sort</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/ui/mainwindow.ui" line="136"/>
-        <location filename="../src/ui/mainwindow.ui" line="253"/>
+        <location filename="../src/ui/mainwindow.ui" line="140"/>
+        <location filename="../src/ui/mainwindow.ui" line="257"/>
         <source>Configure</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/ui/mainwindow.ui" line="143"/>
+        <location filename="../src/ui/mainwindow.ui" line="147"/>
         <source>&amp;Run!</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/ui/mainwindow.ui" line="191"/>
+        <location filename="../src/ui/mainwindow.ui" line="195"/>
         <source>&amp;File</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/ui/mainwindow.ui" line="200"/>
+        <location filename="../src/ui/mainwindow.ui" line="204"/>
         <source>Checking</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/ui/mainwindow.ui" line="207"/>
+        <location filename="../src/ui/mainwindow.ui" line="211"/>
         <source>&amp;Help</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/ui/mainwindow.ui" line="219"/>
+        <location filename="../src/ui/mainwindow.ui" line="223"/>
         <source>&amp;Open Source File</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/ui/mainwindow.ui" line="222"/>
+        <location filename="../src/ui/mainwindow.ui" line="226"/>
         <source>Ctrl+O</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/ui/mainwindow.ui" line="227"/>
+        <location filename="../src/ui/mainwindow.ui" line="231"/>
         <source>&amp;Quit</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/ui/mainwindow.ui" line="230"/>
+        <location filename="../src/ui/mainwindow.ui" line="234"/>
         <source>Ctrl+Q</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/ui/mainwindow.ui" line="235"/>
+        <location filename="../src/ui/mainwindow.ui" line="239"/>
         <source>About &amp;Qt</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/ui/mainwindow.ui" line="240"/>
+        <location filename="../src/ui/mainwindow.ui" line="244"/>
         <source>&amp;About</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/ui/mainwindow.ui" line="245"/>
+        <location filename="../src/ui/mainwindow.ui" line="249"/>
         <source>Check!</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/ui/mainwindow.ui" line="248"/>
+        <location filename="../src/ui/mainwindow.ui" line="252"/>
         <source>F9</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/ui/mainwindow.ui" line="256"/>
+        <location filename="../src/ui/mainwindow.ui" line="260"/>
         <source>F4</source>
         <translation type="unfinished"></translation>
     </message>
