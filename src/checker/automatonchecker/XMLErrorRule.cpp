jirislaby/stansec

commit 422298c0937ee51f3eabc8fbd9e8c617fbe73933

all: use only QList, not QVector
Since qt6, QVector is an alias to QList. So use the latter everywhere.

src/checker/automatonchecker/XMLErrorRule.cpp
@@ -8,7 +8,6 @@
 #include <QList>
 #include <QPair>
 #include <QString>
-#include <QVector>
 
 #include "AutomatonState.h"
 #include "XMLErrorRule.h"
@@ -102,7 +101,7 @@ bool XMLErrorRule::checkIncludedStates(const QSet<AutomatonState> &statesCollect
     const auto &flags = getIncludedMatchFlags();
     const int numIncluded = flags.size();
 
-    QVector<bool> matchingFlags(numIncluded);
+    QList<bool> matchingFlags(numIncluded);
 
     for (auto state : statesCollection) {
 	//            if (isExitRule() && state.getContext().size() > 1)
@@ -118,7 +117,7 @@ bool XMLErrorRule::checkIncludedStates(const QSet<AutomatonState> &statesCollect
 }
 
 bool XMLErrorRule::stateMatch(const AutomatonState &state,
-			      const QString &symbol, const QVector<bool> &flags,
+			      const QString &symbol, const QList<bool> &flags,
 			      const SimpleAutomatonID &simpleID) {
 	const auto &composedID = state.getAutomatonID();
 	const auto &simpleIDs = composedID.getSimpleAutomataIDs();
@@ -149,7 +148,7 @@ XMLErrorRule::buildMatchFlags(const QList<XMLRuleStringParser::XMLRule> &symbols
 		    continue;
 
 	    const auto &vars = std::get<1>(symbol);
-	    QVector<bool> flags;
+	    QList<bool> flags;
 	    flags.reserve(vars.size());
 
 	    for (const auto &var : vars)

src/checker/CheckerError.h
@@ -19,7 +19,6 @@
 
 #include <QList>
 #include <QString>
-#include <QVector>
 
 #include "../codestructures/LazyInternalStructures.h"
 

src/checker/CheckerErrorTrace.cpp
@@ -10,8 +10,8 @@
 #include <clang/StaticAnalyzer/Core/PathSensitive/AnalysisManager.h>
 
 #include <QDebug>
+#include <QList>
 #include <QString>
-#include <QVector>
 
 #include "../codestructures/LazyInternalStructures.h"
 

src/checker/automatonchecker/ComposedAutomatonID.h
@@ -11,7 +11,6 @@
 #include <QDebug>
 #include <QList>
 #include <QString>
-#include <QVector>
 
 #include "SimpleAutomatonID.h"
 
@@ -27,9 +26,9 @@ public:
     }
 
     explicit ComposedAutomatonID(const QList<SimpleAutomatonID> &ids) :
-	    automataIDs(QVector<SimpleAutomatonID>::fromList(ids)) { }
+	    automataIDs(QList<SimpleAutomatonID>::fromList(ids)) { }
 
-    const QVector<SimpleAutomatonID> getSimpleAutomataIDs() const {
+    const QList<SimpleAutomatonID> getSimpleAutomataIDs() const {
         return automataIDs;
     }
 
@@ -41,9 +40,9 @@ public:
     }
 
 private:
-    QVector<SimpleAutomatonID> &getSimpleAutomataIDs() { return automataIDs; }
+    QList<SimpleAutomatonID> &getSimpleAutomataIDs() { return automataIDs; }
 
-    QVector<SimpleAutomatonID> automataIDs;
+    QList<SimpleAutomatonID> automataIDs;
 };
 
 inline bool operator==(const ComposedAutomatonID &lhs,

src/checker/automatonchecker/ErrorTracesListCreator.h
@@ -8,9 +8,8 @@
  * Licensed under GPLv2.
  */
 
-//#include <QList>
+#include <QList>
 #include <QString>
-#include <QVector>
 
 #include "../../codestructures/traversal/CFGPathVisitor.h"
 
@@ -42,8 +41,8 @@ public:
 	rule(rule), transferor(transferor), NLD(NLD), startNode(startNode),
 	internals(internals), detectors(detectors), monitor(monitor) {}
 
-    //QVector<CheckerErrorTrace> getErrorTracesList() const { return errorTracesList; }
-    QVector<CheckerErrorTrace> &getErrorTracesList() { return errorTracesList; }
+    //QList<CheckerErrorTrace> getErrorTracesList() const { return errorTracesList; }
+    QList<CheckerErrorTrace> &getErrorTracesList() { return errorTracesList; }
     int getTotalImportance() const { return totalImportance; }
     QString getFailMessage() const { return failMsg; }
 
@@ -106,7 +105,7 @@ private:
     const NodeLocationDictionary &NLD;
     Stmt startNode;
     const codestructs::LazyInternalStructures &internals;
-    QVector<CheckerErrorTrace> errorTracesList;
+    QList<CheckerErrorTrace> errorTracesList;
     /*const QList<FalsePositivesDetector>*/void *detectors;
     AutomatonCheckerLogger &monitor;
     int totalImportance = FalsePositivesDetector::getBugDefaultImportance();

src/checker/automatonchecker/NodeLocationDictionary.h
@@ -9,9 +9,9 @@
 
 #include <llvm/ADT/Optional.h>
 
+#include <QList>
 #include <QMap>
 #include <QPair>
-#include <QVector>
 
 namespace clang {
 class CFGBlock;
@@ -32,7 +32,7 @@ class PatternLocation;
  */
 using LocationPair = QPair<PatternLocation *, PatternLocation *>;
 /* A vector of (index in CFGBlock + location) */
-using Locations = QVector<QPair<int, LocationPair>>;
+using Locations = QList<QPair<int, LocationPair>>;
 
 class NodeLocationDictionary : public QMap<const clang::CFGBlock *, Locations> {
 public:

src/checker/automatonchecker/PatternLocation.cpp
@@ -11,7 +11,6 @@
 #include <QPair>
 #include <QStack>
 #include <QString>
-#include <QVector>
 
 #include "AutomatonStateCFGcontextAlgo.h"
 #include "AutomatonStateTransferManager.h"
@@ -93,8 +92,8 @@ void PatternLocation::propagateStateToLocation(const AutomatonState &state,
 void PatternLocation::reduceStateSet(QSet<AutomatonState> &set)
 {
 #if 0
-    QVector<AutomatonState> stateVec;
-    QVector<QStack<CFGNode>> contextVec;
+    QList<AutomatonState> stateVec;
+    QList<QStack<CFGNode>> contextVec;
     for (const auto state : set) {
 	stateVec.push_back(state);
 	contextVec.push_back(AutomatonStateCFGcontextAlgo::getContext(state.getContext()));

src/checker/automatonchecker/XMLAutomatonDefinition.cpp
@@ -9,7 +9,6 @@
 #include <QList>
 #include <QMap>
 #include <QString>
-#include <QVector>
 
 #include "XMLAutomatonDefinition.h"
 

commit 4179699bc184c8f43330ecb230a871ebf67c1607

checker: fix fallout from QVector -> QList conversion, part II.
Qt5's QList doesn't support resize() yet, so it ends up with an error:
  XMLErrorRule.cpp:104:42: error: no matching function for call to 'QList<bool>::QList(const int&)'
     QList<bool> matchingFlags(numIncluded);

Fix this by using QVector as it makes more sense here.

Fixes: 422298c0937e ("all: use only QList, not QVector")

src/checker/automatonchecker/XMLErrorRule.cpp
@@ -8,6 +8,7 @@
 #include <QList>
 #include <QPair>
 #include <QString>
+#include <QVector>
 
 #include "AutomatonState.h"
 #include "XMLErrorRule.h"
@@ -101,7 +102,7 @@ bool XMLErrorRule::checkIncludedStates(const QSet<AutomatonState> &statesCollect
     const auto &flags = getIncludedMatchFlags();
     const int numIncluded = flags.size();
 
-    QList<bool> matchingFlags(numIncluded);
+    QVector<bool> matchingFlags(numIncluded);
 
     for (auto state : statesCollection) {
 	//            if (isExitRule() && state.getContext().size() > 1)