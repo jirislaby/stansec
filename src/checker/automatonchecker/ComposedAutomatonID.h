jirislaby/stansec

commit 422298c0937ee51f3eabc8fbd9e8c617fbe73933

all: use only QList, not QVector
Since qt6, QVector is an alias to QList. So use the latter everywhere.

src/checker/automatonchecker/ComposedAutomatonID.h
@@ -11,7 +11,6 @@
 #include <QDebug>
 #include <QList>
 #include <QString>
-#include <QVector>
 
 #include "SimpleAutomatonID.h"
 
@@ -27,9 +26,9 @@ public:
     }
 
     explicit ComposedAutomatonID(const QList<SimpleAutomatonID> &ids) :
-	    automataIDs(QVector<SimpleAutomatonID>::fromList(ids)) { }
+	    automataIDs(QList<SimpleAutomatonID>::fromList(ids)) { }
 
-    const QVector<SimpleAutomatonID> getSimpleAutomataIDs() const {
+    const QList<SimpleAutomatonID> getSimpleAutomataIDs() const {
         return automataIDs;
     }
 
@@ -41,9 +40,9 @@ public:
     }
 
 private:
-    QVector<SimpleAutomatonID> &getSimpleAutomataIDs() { return automataIDs; }
+    QList<SimpleAutomatonID> &getSimpleAutomataIDs() { return automataIDs; }
 
-    QVector<SimpleAutomatonID> automataIDs;
+    QList<SimpleAutomatonID> automataIDs;
 };
 
 inline bool operator==(const ComposedAutomatonID &lhs,

src/checker/CheckerError.h
@@ -19,7 +19,6 @@
 
 #include <QList>
 #include <QString>
-#include <QVector>
 
 #include "../codestructures/LazyInternalStructures.h"
 

src/checker/CheckerErrorTrace.cpp
@@ -10,8 +10,8 @@
 #include <clang/StaticAnalyzer/Core/PathSensitive/AnalysisManager.h>
 
 #include <QDebug>
+#include <QList>
 #include <QString>
-#include <QVector>
 
 #include "../codestructures/LazyInternalStructures.h"
 

src/checker/automatonchecker/ErrorTracesListCreator.h
@@ -8,9 +8,8 @@
  * Licensed under GPLv2.
  */
 
-//#include <QList>
+#include <QList>
 #include <QString>
-#include <QVector>
 
 #include "../../codestructures/traversal/CFGPathVisitor.h"
 
@@ -42,8 +41,8 @@ public:
 	rule(rule), transferor(transferor), NLD(NLD), startNode(startNode),
 	internals(internals), detectors(detectors), monitor(monitor) {}
 
-    //QVector<CheckerErrorTrace> getErrorTracesList() const { return errorTracesList; }
-    QVector<CheckerErrorTrace> &getErrorTracesList() { return errorTracesList; }
+    //QList<CheckerErrorTrace> getErrorTracesList() const { return errorTracesList; }
+    QList<CheckerErrorTrace> &getErrorTracesList() { return errorTracesList; }
     int getTotalImportance() const { return totalImportance; }
     QString getFailMessage() const { return failMsg; }
 
@@ -106,7 +105,7 @@ private:
     const NodeLocationDictionary &NLD;
     Stmt startNode;
     const codestructs::LazyInternalStructures &internals;
-    QVector<CheckerErrorTrace> errorTracesList;
+    QList<CheckerErrorTrace> errorTracesList;
     /*const QList<FalsePositivesDetector>*/void *detectors;
     AutomatonCheckerLogger &monitor;
     int totalImportance = FalsePositivesDetector::getBugDefaultImportance();

src/checker/automatonchecker/NodeLocationDictionary.h
@@ -9,9 +9,9 @@
 
 #include <llvm/ADT/Optional.h>
 
+#include <QList>
 #include <QMap>
 #include <QPair>
-#include <QVector>
 
 namespace clang {
 class CFGBlock;
@@ -32,7 +32,7 @@ class PatternLocation;
  */
 using LocationPair = QPair<PatternLocation *, PatternLocation *>;
 /* A vector of (index in CFGBlock + location) */
-using Locations = QVector<QPair<int, LocationPair>>;
+using Locations = QList<QPair<int, LocationPair>>;
 
 class NodeLocationDictionary : public QMap<const clang::CFGBlock *, Locations> {
 public:

src/checker/automatonchecker/PatternLocation.cpp
@@ -11,7 +11,6 @@
 #include <QPair>
 #include <QStack>
 #include <QString>
-#include <QVector>
 
 #include "AutomatonStateCFGcontextAlgo.h"
 #include "AutomatonStateTransferManager.h"
@@ -93,8 +92,8 @@ void PatternLocation::propagateStateToLocation(const AutomatonState &state,
 void PatternLocation::reduceStateSet(QSet<AutomatonState> &set)
 {
 #if 0
-    QVector<AutomatonState> stateVec;
-    QVector<QStack<CFGNode>> contextVec;
+    QList<AutomatonState> stateVec;
+    QList<QStack<CFGNode>> contextVec;
     for (const auto state : set) {
 	stateVec.push_back(state);
 	contextVec.push_back(AutomatonStateCFGcontextAlgo::getContext(state.getContext()));

src/checker/automatonchecker/XMLAutomatonDefinition.cpp
@@ -9,7 +9,6 @@
 #include <QList>
 #include <QMap>
 #include <QString>
-#include <QVector>
 
 #include "XMLAutomatonDefinition.h"
 

src/checker/automatonchecker/XMLAutomatonDefinition.h
@@ -10,7 +10,6 @@
 #include <QList>
 #include <QMap>
 #include <QString>
-#include <QVector>
 
 #include "../../utils/xmlpatterns/XMLPattern.h"
 
@@ -33,7 +32,7 @@ public:
     QString getAutomatonName() const { return automatonName; }
     QString getAutomatonDescription() const { return automatonDesc; }
     QString getStartSymbol() const { return startSymbol; }
-    const QVector<utils::XMLPattern> &getXMLpatterns() const { return XMLpatterns; }
+    const QList<utils::XMLPattern> &getXMLpatterns() const { return XMLpatterns; }
 
     QList<const XMLTransitionRule *>
     getXMLtransitionRulesForPattern(const utils::XMLPattern *pattern) const {
@@ -64,9 +63,9 @@ private:
     QString automatonDesc;
     QString startSymbol;
 
-    QVector<utils::XMLPattern> XMLpatterns;
-    QVector<XMLTransitionRule> XMLtransitionRules;
-    QVector<XMLErrorRule> XMLerrorRules;
+    QList<utils::XMLPattern> XMLpatterns;
+    QList<XMLTransitionRule> XMLtransitionRules;
+    QList<XMLErrorRule> XMLerrorRules;
 
     QMap<const utils::XMLPattern *, QList<const XMLTransitionRule *>> patternTransitionRulesDictionary;
     QMap<const utils::XMLPattern *, QList<const XMLErrorRule *>> patternErrorRulesDictionary;

commit c4523a8fe4c7ac5be29a9f2912f796c638e49f3c

checker: fix fallout from QVector -> QList conversion
Qt6 doesn't care, but Qt5 does:
error: 'fromList' is not a member of 'QList<checker::SimpleAutomatonID>'

Fix this -- we need no more conversions.

Fixes: 422298c0937e ("all: use only QList, not QVector")

src/checker/automatonchecker/ComposedAutomatonID.h
@@ -26,7 +26,7 @@ public:
     }
 
     explicit ComposedAutomatonID(const QList<SimpleAutomatonID> &ids) :
-	    automataIDs(QList<SimpleAutomatonID>::fromList(ids)) { }
+	    automataIDs(ids) { }
 
     const QList<SimpleAutomatonID> getSimpleAutomataIDs() const {
         return automataIDs;